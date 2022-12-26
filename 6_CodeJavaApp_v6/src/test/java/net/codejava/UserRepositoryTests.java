d31f28722e8a940bb38ba6fa108f650cfbf70539
Firstname Lastname <57352940+SherkhanTazhibayev@users.noreply.github.com>
2022-12-26 23:44:05 +0600

    Add files via upload

---
+++ 6_CodeJavaApp_v6/src/test/java/net/codejava/UserRepositoryTests.java
@@ -0,0 +1,36 @@
+package net.codejava;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+    @Autowired
+    private UserRepository repo;
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Test
+    public void testCreateUser(){
+        User user = new User();
+        user.setEmail("t11a12zhib1ayevs1he2222rkhan7@gmail.com");
+        user.setPassword("sherkhan2000");
+        user.setFirstName("Sherkhan");
+        user.setLastName("Tazhibayev");
+
+        User savedUser = repo.save(user);
+
+        User existUser = entityManager.find(User.class, savedUser.getId());
+
+        assertThat(existUser.getEmail()).isEqualTo(user.getEmail());
+    }
+}
