165970cc522e6eaed056eec42b005f54ee9934f0
Firstname Lastname <57352940+SherkhanTazhibayev@users.noreply.github.com>
2023-01-13 19:43:31 +0600

    Add files via upload

---
+++ security/src/main/java/com/alibou/security/user/User.java
@@ -0,0 +1,22 @@
+package com.alibou.security.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Integer id;
+
+    private String firstname;
+
+    private String lastname;
+
+    private String email;
+
+    private String password;
+}
